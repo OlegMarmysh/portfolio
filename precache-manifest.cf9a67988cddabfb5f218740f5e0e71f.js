6af9b529853c8764a43d5473396091c2328e7b6e
Bbara <you@example.com>
2019-10-15 23:15:28 +0300

    Updates

---
+++ precache-manifest.cf9a67988cddabfb5f218740f5e0e71f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e2dc6090ed1a1607be114fe6068ac5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b2a1c881117b329adf9d",
+    "url": "/portfolio/static/css/main.59e9362b.chunk.css"
+  },
+  {
+    "revision": "a60229180fdfa364d243",
+    "url": "/portfolio/static/js/2.dba36fb8.chunk.js"
+  },
+  {
+    "revision": "b2a1c881117b329adf9d",
+    "url": "/portfolio/static/js/main.ea0f64e7.chunk.js"
+  },
+  {
+    "revision": "6d1202826ed853663cfc",
+    "url": "/portfolio/static/js/runtime-main.ead92456.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);

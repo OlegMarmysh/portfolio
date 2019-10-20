0b04f33edf38cf934644a65809cad38a32728f7a
Bbara <you@example.com>
2019-10-20 17:29:51 +0300

    'add_photo_on_main_page'

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,20 +2,22 @@ import React from 'react';
 import styles from './Main.module.css';
 
 function Main() {
-  return (
-    <div className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.greeting}>
-          <span>Hello, my name is</span>
-          <span>Oleg <span>Marmysh</span></span>
-          <h1>I am front-end developer</h1>
+    return (
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.greeting}>
+                    <span>Hello, my name is</span>
+                    <span>Oleg <span>Marmysh</span></span>
+                    <h1>I am front-end developer</h1>
+                </div>
+                <div className={styles.photo}>
+                    <div className={styles.image}>
+                      <img className={styles.image} src="https://sun9-55.userapi.com/c629212/v629212999/28ef8/conE5Y9WgA8.jpg" alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className={styles.photo}>
-          <div className={styles.image}/>
-        </div>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default Main;
